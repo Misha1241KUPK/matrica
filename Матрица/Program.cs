f1388f7c4470463eac7fc1fa7abaca444fbbbff8
миша миша <mhailovcinnikov2@gmail.com>
2020-09-08 14:42:32 +0300

    Добавьте файлы проекта.

---
+++ Матрица/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Матрица
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[,] matrica = new int[4, 4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (i + j== 3)
+                    {
+                        matrica[i, j] = 1;
+                    }
+                    else
+                    {
+                        matrica[i, j] = 0;
+                    }
+
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(matrica[i, j].ToString() + "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
